2843fd09dd14f1c8791854225f6d6a4344de813d
Kyle <kkndlovu9@gmail.com>
2025-12-01 15:08:48 +0200

    update

--- main.py
+++ main.py
@@ -165,7 +165,7 @@ def _content_security_policy():
         "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; "
         "font-src 'self' https://fonts.gstatic.com; "
         "img-src 'self' data:; "
-        "connect-src 'self' https://accounts.google.com https://www.googleapis.com https://oompiet.space/rag; "
+        "connect-src 'self' https://accounts.google.com https://www.googleapis.com https://oompiet.space/rag https://mentormate-client.vercel.app/; "
         "frame-src https://accounts.google.com;"
     )
 
